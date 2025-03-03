20e217449d1c8d2762d579d35d682e4805443557
LukasSchmu <98956666+LukasSchmu@users.noreply.github.com>
2025-03-03 21:45:55 +0100

    script.js aktualisieren

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const msalConfig = {
     auth: {
-        clientId: "e1e9ecc2-2f89-4640-aa8a-7d9ae189784c
-", // Deine Microsoft-App-Client-ID
-        authority: "https://login.microsoftonline.com/common",
-        redirectUri: "/testuniverse/HTML/main.html" // Nach erfolgreichem Login hierhin weiterleiten
+        clientId: "e1e9ecc2-2f89-4640-aa8a-7d9ae189784c",
+        authority: "https://login.microsoftonline.com/3bfeb222-e42c-4535-aace-ea6f7751369b",
+        redirectUri: window.location.href
     }
+};
+const loginRequest = {
+    scopes: ["https://analysis.windows.net/powerbi/api"]
 };
 
 const msalInstance = new msal.PublicClientApplication(msalConfig);
-
-document.getElementById("sso-login-button").addEventListener("click", function() {
-    msalInstance.loginRedirect({ scopes: ["User.Read"] });
-});
+msalInstance.loginRedirect(loginRequest);
