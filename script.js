fcf47669db596b47aff63ca160f2bcadcbf08781
LukasSchmu <98956666+LukasSchmu@users.noreply.github.com>
2025-03-04 07:23:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById("sso-login-button").addEventListener("click", function() {
     // Weiterleitung auf die index.html
-    window.location.href = "../HTML/main.html";
+    window.location.href = "testuniverse/HTML/main.html";
 });
